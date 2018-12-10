4253b440e495bdb4e95ba6f330082876c04d2084
Pavement <dallsalad@naver.com>
2018-12-10 22:30:49 +0900

    Interceptor, Error Handling

--- complete/src/main/java/hello/WebConfig.java
+++ complete/src/main/java/hello/WebConfig.java
@@ -1,13 +1,11 @@
 package hello;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig  implements WebMvcConfigurer {
 
     private Interceptor interceptor;
